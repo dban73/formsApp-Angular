2e4eef8de05436a238089feb3f3012b0c3975eec
Alejandro <dban095@hotmail.com>
2024-01-08 21:04:54 -0400

    add forms

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register',
+    children: [
+      { path: 'sign-up', component: RegisterComponent },
+      { path: '**', redirectTo: 'sign-up' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
